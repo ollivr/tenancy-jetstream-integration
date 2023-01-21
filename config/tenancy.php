b7e281848d2a06f9ee1dbf164314719ea1765554
Ollivr <66511791+ollivr@users.noreply.github.com>
2023-01-20 18:08:45 -0800

    added

--- config/tenancy.php
+++ config/tenancy.php
@@ -35,7 +35,7 @@ return [
      * Only relevant if you're using the domain or subdomain identification middleware.
      */
     'central_domains' => [
-        'tenancy-jetstream-integration.test'
+        env('CENTRAL_URL')
     ],
 
     'identification' => [
